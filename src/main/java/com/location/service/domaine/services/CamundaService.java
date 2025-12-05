1b07df2c99cf3b8a1b228a56940b41bd1e309486
unknown <diallodiang>
2025-12-05 10:49:56 +0000

    mappers

--- src/main/java/com/location/service/domaine/services/CamundaService.java
+++ src/main/java/com/location/service/domaine/services/CamundaService.java
@@ -3,17 +3,18 @@ package com.location.service.domaine.services;
 import java.util.List;
 import java.util.Map;
 
-import okhttp3.internal.concurrent.Task;
-
 public interface CamundaService {
 
-    void demarrerProcessus(String processDefinitionKey, String businessKey);
+    static void demarrerProcessus(String processDefinitionKey, String businessKey) {
+		// TODO Auto-generated method stub
+		
+	}
 
     Void assignerTache(String idTache, String userId);
 
     void completerTache(String idTache, Map<String, Object> variables);
 
-    List<Task> listeTaches(String userId);
+    List<org.camunda.bpm.engine.task.Task> listeTaches(String userId);
 
     void envoyerMessage(String messageName, String businessKey, Map<String, Object> variables);
 }
